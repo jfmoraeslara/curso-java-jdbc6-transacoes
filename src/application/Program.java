72b2a6b6e04e6c14a6713e8b6a3fe92def05f554
João Fernando <jfmoraeslara@gmail.com>
2022-04-03 12:44:43 -0300

    Protegendo transações

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
@@ -15,24 +16,29 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 			
-			// transações
+			// proteger as transações
+			conn.setAutoCommit(false);
+			
 			st = conn.createStatement();
 			
 			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 			
-			int x = 1;
-			if (x < 2) {
-				throw new SQLException("Fake error");
-			}
-			
 			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			
+			// confirmar a transação
+			conn.commit();
 			
 			System.out.println("rows 1 " + rows1);
 			System.out.println("rows 2 " + rows2);
 			
 		}
 		catch (SQLException e) {
-			e.printStackTrace();
+			try {
+				conn.rollback();
+				throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
+			}
 		}
 		finally {
 			DB.closeStatement(st);
